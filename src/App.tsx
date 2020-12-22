d9ead0c342c2fc5d0b766484879c96e182dfe403
wason <ninjinx8888@gmail.com>
2020-12-22 23:52:19 +0900

    add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import {FormControl, TextField} from "@material-ui/core";
 import "./App.css";
 import { db } from "./firebase";
 
 const App: React.FC = () => {
   const [tasks, setTasks] = useState([{ id: "", title: "" }]);
+  const [input, setInput] = useState("");
+
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot) => {
       setTasks(
@@ -18,6 +21,17 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
+      <h1>react-todo-demo</h1>
+      <FormControl>
+        <TextField
+          InputLabelProps={{
+            shrink: true,
+          }}
+          label = "New task?"
+          value = {input}
+          onChange = {(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
+        />
+      </FormControl>
       {tasks.map((task) => (
         <h3 key={task.id}>{task.title}</h3>
       ))}
